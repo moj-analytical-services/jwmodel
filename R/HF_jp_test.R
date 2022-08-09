d7d5e15138e7c3bf1c528de59831a2f026284438
henryk01trent <henryk.faas@justice.gov.uk>
2022-08-09 13:58:15 +0000

    cleaned up, reads data as of 9 august

--- R/HF_jp_test.R
+++ R/HF_jp_test.R
@@ -5,13 +5,13 @@
 # ---- Install package manager renv ----
 # see instructions in moj analytical
 
-# ---- install Rs3tools package to load data from s3 ----
+# install Rs3tools package to load data from s3 
 # renv::install("moj-analytical-services/Rs3tools")
 
 # library(remotes)  # connect to GitHub
 # library(botor)    # connect to cloud
 # renv::install("readxl")
-renv::install("janitor")
+# renv::install("janitor")
 library(tidyverse)
 library(readxl)
 library(openxlsx)
@@ -37,11 +37,15 @@ renv::snapshot()
 # renv::restore()
 
 # ---- I. Loading assumptions file ----
+input_file_name <- "Judicial S&D input hf.xlsx"
+input_file <- paste0("alpha-testhf/jmw_assumptions/", input_file_name)
+# loading data input as input_file.xlsx from s3 bucket (working)
+# Rs3tools::download_file_from_s3("alpha-testhf/jmw_assumptions/2022-08-04 Assumptions JCG Sept.xlsx", "input_file.xlsx", overwrite =TRUE)
+Rs3tools::download_file_from_s3(input_file, "input_file.xlsx", overwrite =TRUE)
+# Rs3tools::download_file_from_s3("alpha-testhf/jmw_assumptions/Judicial S&D input.xlsx", "input_file.xlsx", overwrite =TRUE)
+
 # show all files in s3 bucket testhf 
 botor::s3_ls("s3://alpha-testhf")
-
-# loading data input as input_file.xlsx from s3 bucket (working)
-Rs3tools::download_file_from_s3("alpha-testhf/jmw_assumptions/2022-08-04 Assumptions JCG Sept.xlsx", "input_file.xlsx", overwrite =TRUE)
 
 # Defining an S3 class for jwmodel 
 source("R/jwmodel.R")
@@ -70,10 +74,7 @@ obj <- optimise(obj)
 # all results of simulation
 result_temp <- obj$outputs
 
-# resource result (judge, status, FTE)
-df_res <- as.data.frame(obj$outputs$resource_output)
-
-# allocation result
+# ---- Allocation result ----
 df <- as.data.frame(obj$outputs$allocation_output)
 
 # save to csv file
@@ -92,6 +93,25 @@ ggplot(dfb, aes(x = year, y = alloc, group = jurisdiction)) +
          geom_line(aes(linetype=jurisdiction, color=jurisdiction))+
          geom_point(aes(color=jurisdiction))+
          theme(legend.position="top")         
-         
 
 
+# ---- Resource result ----         
+# resource result (judge, status, fte)
+df_res <- as.data.frame(obj$outputs$resource_output)
+
+# save to csv file
+write_csv(df_res, "./result/result_df_resource.csv")
+
+# analyze data
+dfa_res <- as.data.frame(janitor::clean_names(df_res)) %>% 
+  mutate(fte = round(fte, digits =0)) 
+
+dfb_res <- dfa_res %>% select(year, judge, status,  fte) %>% 
+  group_by(judge, status, year ) %>%
+  summarize(fte = sum(fte))
+
+# plot result allocations
+ggplot(dfb_res, aes(x = year, y = fte, group = judge)) + 
+  geom_line(aes(linetype=judge, color=judge))+
+  geom_point(aes(color=judge))+
+  theme(legend.position="top")   
